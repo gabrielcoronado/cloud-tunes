5ebe0a657400be870ccbaff3a8646c62b9180605
Gabriel Coronado <gabrielcoronado@x.local>
2023-01-05 17:03:36 -0500

    Initial skeleton and setup

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1,3 +1,41 @@
-const Discover = () => <div>Discover</div>;
+import { Error, Loader, SongCard } from "../components";
+import { genres } from "../assets/constants";
+import { useGetTopChartsQuery } from "../redux/services/shazamCore";
+
+const Discover = () => {
+  const { data, isFetching, error } = useGetTopChartsQuery();
+  const genreTitle = "Pop";
+  console.log(data);
+
+  if (isFetching) return <Loader title="Loading songs..." />;
+  if (error) return <Error />;
+
+  return (
+    <div className="flex flex-col">
+      <div className="w-full flex justify-between items-center sm:flex-row flex-col mt-4 mb-10">
+        <h2 className="font-bold text-3xl text-white text-left">
+          Discover {genreTitle}
+        </h2>
+        <select
+          onChange={() => {}}
+          value=""
+          className="bg-black text-gray-300 p-3 rounded-lg text-sm outline-none sm:mt-0 mt-5"
+        >
+          {genres.map((genre) => (
+            <option value={genre.value} key={genre.value}>
+              {genre.title}
+            </option>
+          ))}
+        </select>
+      </div>
+      {/* SONGS WRAPPER */}
+      <div className="flex flex-wrap sm:justify-start justify-center gap-8">
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((song, i) => (
+          <SongCard key={i} song={song} i={i} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default Discover;
